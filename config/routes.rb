a5b3024882041d4ca3f0ffcc266ee890ee2208da
Cody Swann <cody@gunnertech.com>
2012-07-20 18:55:28 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Recoverytracker::Application.routes.draw do
+  get "users/index"
+
+  get "users/show"
+
+  get "users/create"
+
+  get "users/update"
+
+  get "users/destroy"
+
+  get "users/new"
+
+  get "users/edit"
+
   devise_for :users
   authenticated :user do
     root :to => 'home#index'
