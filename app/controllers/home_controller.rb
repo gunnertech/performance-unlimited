532f03228de56a809b4a54d1ca6c23e58f38e243
Cody Swann <cody@gunnertech.com>
2012-07-26 21:41:23 -0400

    work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,7 @@ class HomeController < ApplicationController
     @division = @organization.divisions.find_by_name('Pittsburgh Pirates')
     @groups = @division.groups
     @users = @division.users
-    @grouped_users = []
+    @grouped_users = @division.grouped_users
     @survey = @division.surveys.first
-    
-    0.upto((@users.size/@groups.count).ceil-1).each do |row|
-      @grouped_users[row] ||= []
-      @groups.each_with_index do |group,i|
-        @grouped_users[row][i] ||= []
-        @grouped_users[row][i].push(@users.joins{ groups }.where{ groups.id >> group.id }.to_a)
-        @grouped_users[row][i].flatten!
-      end
-    end
   end
 end
