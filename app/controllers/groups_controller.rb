8f4800b3b10951151f3bc4132188e83f71909146
Cody Swann <cody@gunnertech.com>
2012-08-02 13:37:06 -0400

    lots of work

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,14 @@
 class GroupsController < InheritedResources::Base
   belongs_to :division, optional: true
   respond_to :json
+  
+  def show
+    show! do |success|
+      success.html
+      success.csv {
+        @dates = (30.days.ago.to_date..Date.today).map{ |date| date }
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{parent.name.parameterize}-#{@group.name.parameterize}-#{Date.today}.csv\""
+      }
+    end
+  end
 end
