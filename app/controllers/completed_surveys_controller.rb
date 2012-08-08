d9a62891f35fb887a5b72aab09fdd19aed5af9fe
Cody Swann <cody@gunnertech.com>
2012-08-08 16:56:42 -0400

    minor

--- app/controllers/completed_surveys_controller.rb
+++ app/controllers/completed_surveys_controller.rb
@@ -14,17 +14,20 @@ class CompletedSurveysController < InheritedResources::Base
   
   def create
     @completed_survey = parent.completed_surveys.create(user_id: params[:taker_id].to_i, date: Date.today)
-    @user = User.find(params[:taker_id])
-    params[:responses].split(',').each do |response_code|
-      question_id, response_id = response_code.split('-')
-      @completed_survey.selected_responses.create(user_id: params[:taker_id], response_id: response_id)
-    end
     
-    title = "#{@user.name.parameterize}-#{Date.today}.csv"
-    @dates = (30.days.ago.to_date..Date.today).map{ |date| date }
-    # @user.organizations.each do |organization|
-    #   organization.upload_spreadsheets(render_to_string(template: 'users/show.csv.erb'), title)
-    # end
+    if @completed_survey.valid?
+      @user = User.find(params[:taker_id])
+      params[:responses].split(',').each do |response_code|
+        question_id, response_id = response_code.split('-')
+        @completed_survey.selected_responses.create(user_id: params[:taker_id], response_id: response_id)
+      end
+    
+      title = "#{@user.name.parameterize}-#{Date.today}.csv"
+      @dates = (30.days.ago.to_date..Date.today).map{ |date| date }
+      @user.organizations.each do |organization|
+        organization.upload_spreadsheets(render_to_string(template: 'users/show.csv.erb'), title)
+      end
+    end
     
     create!
   end
