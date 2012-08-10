3a35baf39630788317af39f85a7d5c517a817ce1
Cody Swann <cody@gunnertech.com>
2012-08-10 11:12:30 -0400

    Adds google creds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def icon_image_tag
     if @organization
       image_tag(@organization.logo.url)
-    elsif defined?(:resouce) && resouce && resource.respond_to?(:organization)
+    elsif defined?(:resource) && resource && resource.respond_to?(:organization)
       image_tag(resource.organization.logo.url)
     end
   end
