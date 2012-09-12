fbd5a8b7a0364b42cf9fe9af03b7c22a45646277
Cody Swann <cody@gunnertech.com>
2012-09-12 17:01:25 -0400

    Adds permissions for agencies

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -2,7 +2,7 @@ class DivisionsController < InheritedResources::Base
   belongs_to :organization, optional: true
   custom_actions resource: :leaderboard
   layout :get_layout
-  load_and_authorize_resource except: [:leaderboard]
+  skip_load_and_authorize_resource :leaderboard
   
   def create
     @division = Division.create(params[:division].merge(creator: current_user))
