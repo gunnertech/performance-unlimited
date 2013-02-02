ac5b0c4efb4cc0646401d276988f72e530c85d15
Cody Swann <cody@gunnertech.com>
2013-02-01 22:57:19 -0800

    adds alert notifications

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -5,6 +5,6 @@ class AlertMailer < ActionMailer::Base
     domain = completed_survey.organization.mapped_domains.first.try(:domain) || DEFAULT_DOMAIN
     emails = completed_survey.organization.users.joins{ roles }.where{ roles.name == 'admin' }.select{ email }.map(&:email).uniq
     
-    mail(from: "notifications@#{domain}" :to => emails, :subject => "Survey Notifications for #{completed_survey.user}")
+    mail(from: "notifications@#{domain}", :to => emails, :subject => "Survey Notifications for #{completed_survey.user}")
   end
 end
