0cf989431ed99e6067dffe1b1edc82af1d141397
Cody Swann <cody@gunnertech.com>
2013-03-15 09:31:06 -0400

    closes #3335

--- app/models/assigned_group.rb
+++ app/models/assigned_group.rb
@@ -2,12 +2,15 @@ class AssignedGroup < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
   
+  has_one :division, through: :group
   has_one :organization, through: :group
   attr_accessible :position
   
   acts_as_list scope: :group
   
+  validates :group_id, uniqueness: { scope: :user_id }
+  
   def to_s
-    "#{organization.to_s}: #{group.to_s}"
+    "#{organization.to_s} (#{division.to_s}): #{group.to_s}"
   end
 end
