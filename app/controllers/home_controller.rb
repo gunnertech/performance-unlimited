ab4669a38c21e56aa9e252a3eb676077cf9db515
Cody Swann <cody@gunnertech.com>
2014-07-29 01:02:11 -0400

    minor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @mapped_domain = MappedDomain.find_by_domain(request.host.to_s) || MappedDomain.find_by_domain(DEFAULT_DOMAIN)
     @organization = @mapped_domain.organization
+    authorize! :read, @organization
     
     @possible_organizations = Organization.with_role('admin', current_user)
     @possible_divisions = Division.where{ id >> my{Organization.with_role('admin', current_user).joins{ divisions }.pluck('divisions.id')}}
