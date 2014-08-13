a3c07442ed24aaef43ca0fe9866e951a143f7f21
Cody Swann <cody@gunnertech.com>
2014-08-13 11:34:18 -0400

    minor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @mapped_domain = MappedDomain.find_by_domain(request.host.to_s) || MappedDomain.find_by_domain(DEFAULT_DOMAIN)
+    raise @mapped_domain.inspect
     @organization = @mapped_domain.organization
     authorize! :read, @organization
     
