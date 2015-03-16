ed90184dda3c0473b9040c7623c04aeedb419c55
Cody Swann <cody@gunnertech.com>
2015-03-16 16:14:17 -0400

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ PerformanceUnlimited::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'performance-un-limited.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -80,7 +81,7 @@ PerformanceUnlimited::Application.configure do
   
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
